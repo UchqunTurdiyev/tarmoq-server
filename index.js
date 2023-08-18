fb6b1ce49da694b64564b479b9b92ff3aff5719d
uchqun007 <uchqunjonturdiev@gmail.com>
2023-08-18 17:26:41 +0500

    create  all post create post my post

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const PORT = 5000;
 const { MONGO_URI } = require('./keys');
 
 require('./models/user');
+require('./models/post');
+
+mongoose.connect(MONGO_URI);
 
 app.use(express.json());
 
 app.use(require('./routes/auth'));
-
-mongoose.connect(MONGO_URI);
+app.use(require('./routes/post'));
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT} ....`);
